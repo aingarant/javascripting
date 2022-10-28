60f0a8a32d05e32694f2a99f9ccad122286c02da
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-10-28 00:56:10 -0400

    pushing

--- program.js
+++ program.js
@@ -8,12 +8,11 @@ const c = 3;
 
   (function secondFunction() {
     const b = 8;
-    console.log(`a: ${a}, b: ${b}, c: ${c}`); 
+    console.log(`a: ${a}, b: ${b}, c: ${c}`);
 
     (function thirdFunction() {
       const a = 7;
       const c = 9;
-
       (function fourthFunction() {
         const a = 1;
         const c = 8;
